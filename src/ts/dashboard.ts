686a6a79289489d057dc94ded4726e385123c4b9
badmagick329 <uzairfarooqi@gmail.com>
2025-07-08 21:01:16 +0100

    feat: add task sorting

--- src/ts/dashboard.ts
+++ src/ts/dashboard.ts
@@ -1,25 +1,61 @@
 /**
- * setupDashboard sets up the dashboard to filter tasks by status.
+ * setupDashboard sets up the dashboard to sort and filter tasks
  *
  * @returns {void}
  */
 export function setupDashboard() {
-  const taskStatusFilter = document.getElementById('task-status-filter');
-  if (!taskStatusFilter) return;
-
   const taskCards = document.querySelectorAll('[data-task-status]');
   if (taskCards.length === 0) return;
 
-  taskStatusFilter.addEventListener('change', (event) => {
-    const selectedStatus = (event.target as HTMLSelectElement).value;
-    taskCards.forEach((card) => {
-      const cardStatus = (card as HTMLElement).dataset.taskStatus;
-      if (selectedStatus === 'All' || cardStatus === selectedStatus) {
-        tryShow(card);
-      } else {
-        tryHide(card);
+  const taskStatusFilter = document.getElementById('task-status-filter');
+  if (taskStatusFilter) {
+    taskStatusFilter.addEventListener('change', (event) => {
+      const selectedStatus = (event.target as HTMLSelectElement).value;
+      taskCards.forEach((card) => {
+        const cardStatus = (card as HTMLElement).dataset.taskStatus;
+        if (selectedStatus === 'All' || cardStatus === selectedStatus) {
+          tryShow(card);
+        } else {
+          tryHide(card);
+        }
+      });
+    });
+  }
+
+  const taskSorter = document.getElementById('task-sorter');
+  if (!taskSorter) return;
+
+  taskSorter.addEventListener('change', (event) => {
+    const selectedKey = (event.target as HTMLSelectElement).value;
+    if (!selectedKey) return;
+
+    const container = taskCards[0].parentElement;
+    if (!container) return;
+
+    const cardsArray = Array.from(taskCards) as HTMLElement[];
+    cardsArray.sort((a, b) => {
+      let valA: string = '';
+      let valB: string = '';
+      switch (selectedKey) {
+        case 'title':
+          valA = a.dataset.taskTitle || '';
+          valB = b.dataset.taskTitle || '';
+          return valA.localeCompare(valB);
+        case 'status':
+          valA = a.dataset.taskStatus || '';
+          valB = b.dataset.taskStatus || '';
+          return valA.localeCompare(valB);
+        case 'due_date':
+          valA = a.dataset.taskDueDate || '';
+          valB = b.dataset.taskDueDate || '';
+
+          return new Date(valA).getTime() - new Date(valB).getTime();
+        default:
+          return 0;
       }
     });
+
+    cardsArray.forEach((card) => container.appendChild(card));
   });
 }
 
