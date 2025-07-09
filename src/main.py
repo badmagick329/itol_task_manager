67b703a633d04d4174e79e197422283a4f3a0c3a
badmagick329 <uzairfarooqi@gmail.com>
2025-07-09 11:01:37 +0100

    fix: env loading for secret

--- src/main.py
+++ src/main.py
@@ -17,13 +17,12 @@ if str(BASE_DIR) not in sys.path:
 from src.config import Config  # noqa: E402
 from src.web.app import create_app  # noqa: E402
 
-secret = os.environ.get("SECRET_KEY")
-if secret:
-    Config.inject_secret(secret)
-app = create_app(Config)
-
 if __name__ == "__main__":
     load_dotenv()
+    secret = os.environ.get("SECRET_KEY")
+    if secret:
+        Config.inject_secret(secret)
+    app = create_app(Config)
     use_reloader = os.environ.get("USE_RELOADER") == "1"
     debug = os.environ.get("DEBUG") == "1"
     app.run(debug=debug, use_reloader=use_reloader)
